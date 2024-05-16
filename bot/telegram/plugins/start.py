628932cebf00b60446dc40b8d7408a4e2d224a3e
Anik <135734362+anikalwayspro@users.noreply.github.com>
2024-05-16 21:29:32 +0530

    Update start.py

--- bot/telegram/plugins/start.py
+++ bot/telegram/plugins/start.py
@@ -9,10 +9,9 @@ async def start(bot: Client, message: Message):
         try:
             usr_cmd = message.text.split("_")[-1]
             data = usr_cmd.split("-")
-            message_id, chat_id = data[0], f"-{data[1]}"
-            file = await bot.get_messages(int(chat_id), int(message_id))
+            message_id, chat_id = int(data[0]), int(data[1])
+            file = await bot.get_messages(chat_id, message_id)
             media = is_media(file)
-            await message.reply_cached_media(file_id=media.file_id, caption=f'**{media.file_name}**', protect_content=True)
-          #  protect_content = true
+            await message.reply_cached_media(file_id=media.file_id, caption=f'**{media.file_name}**', disable_notification=True)
         except Exception as e:
             print(f"An error occurred: {e}")
